c8d9c88f1c7cd0e75e9c36639d861dab2d9b921e
Samieh Abdeljabbar <49135655+samiehabdeljabbar@users.noreply.github.com>
2024-02-09 20:44:28 -0600

    Updating another thing

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 #Importing Seleniam and Webdriver
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 #Initializing the Safari Webdriver
 driver = webdriver.Safari()
+
+#open a website
+driver.get("https://en.wikipedia.org/wiki/Apple_Inc.")
+
+#find an element on the webpage
+element = driver.find_element(By.CSS_SELECTOR, 'apple')
+
+#print the text of the found element
+print(element.text)
+
+driver.quit()
