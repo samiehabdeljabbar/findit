a60a0b3bf67eb7155f54bd4eb1f9ad9578dc1162
Samieh Abdeljabbar <49135655+samiehabdeljabbar@users.noreply.github.com>
2024-02-09 22:30:26 -0600

    gace up on safari for now

--- second.py
+++ second.py
@@ -1,5 +1,10 @@
-#Importing Seleniam and Webdriver
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-#Initializing the Safari Webdriver
-driver = webdriver.Safari()
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+    options=options)
