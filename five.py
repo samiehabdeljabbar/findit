fc638d930bf55ecc4ac47de46a23bf4681aaf776
Samieh Abdeljabbar <49135655+samiehabdeljabbar@users.noreply.github.com>
2024-02-10 18:16:18 -0600

    looking at the official documentation

---
+++ five.py
@@ -0,0 +1,19 @@
+# this provides all the webdriver implementations
+from selenium import webdriver
+#The KEYS class provides keys in teh keyboard like RETURN, F1, ALT.
+from selenium.webdriver.common.keys import Keys
+#the BY class is used to locate elements within a document
+from selenium.webdriver.common.by import By
+#import time finction
+import time
+
+#Next we have to implement the instance of Chrome Webdriver
+driver = webdriver.Chrome()
+
+
+
+#the driver.get method will navigate to a webpage by the URL
+
+driver.get("http://www.python.org")
+time.wait(25)
+driver.quit()
